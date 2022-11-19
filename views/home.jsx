1637f20e50f2914fa18567d13c52ae622cfa4e59
Veny Cordova <venycordova@gmail.com>
2022-11-19 13:53:53 -0500

    adding bootstrap to homepage

--- views/home.jsx
+++ views/home.jsx
@@ -6,8 +6,9 @@ function home (){
         <Def>
             <main>
                 <h1>
-                    HOME
+                    REST-Rant
                 </h1>
+                <a href="/places">Places To Rant And Rave About</a>
                 <div>
                     <img src="/images/veggies.jpg" alt="Vegatable Variety" />
                     <div>Photo by <a href="AUTHOR_LINK">Dan Gold</a> on <a href="UNSPLASH_LINK">Unsplash</a></div>
