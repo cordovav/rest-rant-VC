426aaf6ff153e8982e76b347846fa0417662bfbd
Veny Cordova <venycordova@gmail.com>
2022-11-19 14:01:44 -0500

    adding and rendering a new view to add a new place

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,11 @@
 const router = require('express').Router()
 
+
+router.get('/new', (req, res) =>{
+    res.render('places/new')
+})
+
+
 router.get('/', (req, res) => {
     let places = [{
         name: 'H-Thai-ML',
@@ -16,6 +22,8 @@ router.get('/', (req, res) => {
     }]
     res.render('places/index', {places})
 })
+
+
 
 module.exports = router
 
