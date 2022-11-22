faf2114c300eccd117e21d45b632848fda00d093
Veny Cordova <venycordova@gmail.com>
2022-11-21 20:05:08 -0500

    adding style to navbar and body or page

--- views/default.jsx
+++ views/default.jsx
@@ -9,6 +9,13 @@ function Def (html){
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/places">Places</a></li>
+                        <li><a href="/places/new">Add Places</a></li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossOrigin="anonymous"></script>
