8c7c458b4e5244d8956924767d50f36eefe8c9eb
Veny Cordova <venycordova@gmail.com>
2022-12-03 14:21:04 -0500

    creating and using helper method in schema

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -9,6 +9,9 @@ function show (data) {
                     
                     <div className='col-sm-6'>
                         <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
                     </div>
                     <div className='col-sm-6'>
                         <h1>{ data.place.name }</h1>
@@ -16,8 +19,12 @@ function show (data) {
                                 <h3>Not Rated</h3>
                         <br />
                             <h2>Description</h2>
-                                <h3>Located in {data.place.city}, {data.place.state}</h3>
-                                <h3>Serving {data.place.cuisines}</h3>
+                            <h3>
+                                {data.place.showEstablished()}
+                            </h3>
+                                <h4>
+                                    Serving {data.place.cuisines}
+                                    </h4>
                         <br />
                         <a href={`/places/${data.id}/edit`} className='btn btn-warning'> 
                             Edit 
